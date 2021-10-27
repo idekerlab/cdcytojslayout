0648dee92e21a4129a5246a5ff8942b5584be1bd
Chris Churas <churas.camera@gmail.com>
2021-10-27 15:30:30 -0700

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+const { ArgumentParser }  = require('argparse');
+const { version } = require('../package.json');
+
+const fs = require('fs');
+
+var cytoscape = require('cytoscape');
+
+const { CxToJs, CyNetworkUtils } = require('cytoscape-cx2js');
+
+const parser = new ArgumentParser({
+    description: "Runs Cytoscape.js layouts on the command line outputting the node positions to standard out"
+});
+
+parser.add_argument('input', {help: 'File of Network in CX format'});
+parser.add_argument('--version', { action: 'version', version });
+parser.add_argument('--layout', { choices: ['cose', 'grid', 'circle', 'concentric', 'breadthfirst'],
+                                  default: 'cose',
+                                  help: 'Layout algorithm to run'});
+
+var args = parser.parse_args();
+
+var layout = args.layout;
+
+// read CX file
+var content = fs.readFileSync(args.input);
+var rawCX = JSON.parse(content);
+
+// initialize cytoscapeCx2js
+var utils = new CyNetworkUtils();
+var niceCX = utils.rawCXtoNiceCX(rawCX);
+var cx2Js = new CxToJs(utils);
+
+// initialize cytoscape.js
+var cy = cytoscape({});
+
+// add the elements from the CX file into cytoscape.js
+var eles = cy.add(cx2Js.cyElementsFromNiceCX(niceCX, {}));
+
+// function called when layout completed
+var ready_func = function() {
+   var first = true;
+   process.stdout.write('[\n');
+   for (const n of cy.json().elements.nodes){
+        if (first === true){
+           first = false;
+        } else {
+          process.stdout.write(',\n');
+        }
+        process.stdout.write(JSON.stringify({ node: n.data.id,
+                                            x: n.position.x,
+                                            y: n.position.y}));
+    }
+    process.stdout.write('\n]\n');
+    process.exit(0);
+
+};
+
+// configure the layout
+var layout = cy.layout({
+    name: layout,
+    boundingBox: { x1: 0,
+                   y1: 0,
+                   w: 500,
+                   h: 500},
+    ready: ready_func 
+
+});
+
+// run the layout
+layout.run();
+
+
+// denote we ran successfully
+process.exitCode = 0
